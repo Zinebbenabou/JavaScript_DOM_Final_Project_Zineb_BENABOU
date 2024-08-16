ea5c32b73423f3c51be312b339f7554133486223
Zineb Benabou <benabbouzineb1@gmail.com>
2024-08-16 16:25:26 +0100

    rest

--- public/js/app.js
+++ public/js/app.js
@@ -133,5 +133,96 @@ containers.forEach(container => {
     indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator')
    
 });
+// todo gallery 
+let galleryContainer = document.querySelector(".gallery-container");
+let galleryNavContainer = document.querySelector(".gallery-nav");
+let galleryItems = document.querySelectorAll(".gallery-item");
+
+class Carousel1 {
+    constructor(container, items, nav) {
+        this.carouselContainer = container;
+        this.carouselArray = [...items];
+        this.carouselNav = nav;
+        this.currentItemIndex = 0;
+        this.autoSlideInterval = null;
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(ele => {
+            ele.classList.remove("gallery-item-1");
+            ele.classList.remove("gallery-item-2");
+            ele.classList.remove("gallery-item-3");
+            ele.classList.remove("gallery-item-4");
+            ele.classList.remove("gallery-item-5");
+        });
+
+        this.carouselArray.slice(0, 5).forEach((ele, i) => {
+            ele.classList.add(`gallery-item-${i + 1}`);
+        });
+
+        this.updateIndicators();
+    }
+
+    updateIndicators() {
+        if (this.carouselNav && this.carouselNav.childNodes.length > 0) {
+            this.carouselNav.childNodes.forEach(indicator => {
+                if (indicator.classList) {
+                    indicator.classList.remove('active');
+                }
+            });
+            if (this.currentItemIndex < this.carouselNav.childNodes.length) {
+                this.carouselNav.childNodes[this.currentItemIndex].classList.add('active');
+            }
+        }
+    }
+
+    setCurrentState(index) {
+        this.currentItemIndex = index;
+        this.carouselArray = this.carouselArray.slice(index).concat(this.carouselArray.slice(0, index));
+        this.updateGallery();
+    }
+
+    setIndicators() {
+      while (this.carouselNav.firstChild) {
+          this.carouselNav.removeChild(this.carouselNav.firstChild);
+      }
+  
+      this.carouselArray.forEach((_, index) => {
+          let li = document.createElement('li');
+          li.addEventListener('click', () => {
+              this.setCurrentState(index);
+              this.stopAutoSlide(); 
+          });
+          this.carouselNav.appendChild(li);
+      });
+  
+      
+      this.updateIndicators();
+  }
+  
+
+    startAutoSlide() {
+        this.autoSlideInterval = setInterval(() => {
+            this.currentItemIndex = (this.currentItemIndex + 1) % this.carouselArray.length;
+            this.setCurrentState(this.currentItemIndex);
+        }, 2000); 
+    }
+
+    stopAutoSlide() {
+        clearInterval(this.autoSlideInterval);
+    }
+
+    init() {
+        this.updateGallery();
+        this.setIndicators();
+        this.startAutoSlide();
+        
+        this.carouselContainer.addEventListener('mouseover', () => this.stopAutoSlide());
+        this.carouselContainer.addEventListener('mouseout', () => this.startAutoSlide());
+    }
+}
+
+const exampleCarousel = new Carousel1(galleryContainer, galleryItems, galleryNavContainer);
+exampleCarousel.init();
 
 
